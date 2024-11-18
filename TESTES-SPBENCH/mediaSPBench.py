1f114b40dc552350b8d78c0e14bd457ca58ac8c5
sbcSpohr <114929637+sbcSpohr@users.noreply.github.com>
2024-11-18 17:07:43 -0300

    Add files via upload

--- TESTES-SPBENCH/mediaSPBench.py
+++ TESTES-SPBENCH/mediaSPBench.py
@@ -13,7 +13,7 @@ def calcular_media_pandas(prefixo):
             df = pd.read_csv(file_path, delim_whitespace=True)
 
             for thread in [1, 2, 3]:
-                latency_values = df[df['Thread'] == thread]['Average_latency']
+                latency_values = df[df['Thread'] == thread]['Average_exec_time']
                 latencies[thread].extend(latency_values)
 
     media_resultados = {thread: (pd.Series(latencies[thread]).mean() if latencies[thread] else None) for thread in latencies}
@@ -24,18 +24,18 @@ media_lane = calcular_media_pandas('lane')
 media_ferret = calcular_media_pandas('ferret')
 media_person = calcular_media_pandas('person')
 
-print("Média de Average_latency para bzip2:")
+print("Média de Average_exec_time para bzip2:")
 for thread in media_bzip2:
     print(f"  Thread {thread}: {media_bzip2[thread]}")
 
-print("\nMédia de Average_latency para lane:")
+print("\nMédia de Average_exec_time para lane:")
 for thread in media_lane:
     print(f"  Thread {thread}: {media_lane[thread]}")
 
-print("\nMédia de Average_latency para ferret")
+print("\nMédia de Average_exec_time para ferret")
 for thread in media_ferret:
     print(f"  Thread {thread}: {media_ferret[thread]}")
 
-print("\nMédia de Average_latency para person")
+print("\nMédia de Average_exec_time para person")
 for thread in media_person:
     print(f"  Thread {thread}: {media_person[thread]}")
