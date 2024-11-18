01539712aa7c6dc02f66b59de8ebc184c3db69cd
sbcSpohr <114929637+sbcSpohr@users.noreply.github.com>
2024-11-18 02:21:53 -0300

    Update generate_date_csv.sh

--- Metodologia2/generate_date_csv.sh
+++ Metodologia2/generate_date_csv.sh
@@ -11,19 +11,14 @@ OUTPUT_CSV=$3
 
 echo "app_name,num_worker,date,elapsed_time" > "$OUTPUT_CSV"
 
-# Lê o CSV de entrada e processa cada linha
 tail -n +2 "$INPUT_CSV" | while IFS=, read -r app_name num_worker elapsed_time; do
-    # Encontra o arquivo de data que corresponde ao app_name e ao padrão
     date_file=$(find "$DATE_DIR" -name "NPB-$app_name*.txt" | head -n 1)
 
-    # Verifica se o arquivo de data foi encontrado
     if [[ -f "$date_file" ]]; then
-        # Lê uma linha de data do arquivo
         {
-            read -r date || date="N/A"  # Define "N/A" caso não tenha mais datas no arquivo
+            read -r date || date="N/A"  
         } < "$date_file"
         
-        # Escreve no CSV final com a data incluída
         echo "$app_name,$num_worker,$date,$elapsed_time" >> "$OUTPUT_CSV"
     else
         echo "Warning: Arquivo de data não encontrado para $app_name" >&2
